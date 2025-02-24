473386de1fbfc6e12171e74401d8d4071ec8f077
Livitha Ravisangar <Livitha.Ravisangar@ksb-sg.ch>
2025-02-24 11:33:24 +0100

    Projektdateien hinzufügen.

---
+++ TicTacToe_gruppe4/GameBoardModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_gruppe4
+{
+    public class GameBoardModel
+    {
+        private char[,] board;
+
+
+        public GameBoardModel(int size)
+        {
+            board = new char[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    board[i, j] = ' ';
+                }
+            }
+        }
+        public char GetCell(int row, int col)
+        {
+            return board[row, col];
+        }
+
+        public void SetCell(int row, int col, char symbol)
+        {
+            board[row, col] = symbol;
+        } 
+    }
+}
---
+++ TicTacToe_gruppe4Tests/GameBoardModelTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe_gruppe4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_gruppe4.Tests
+{
+    [TestClass()]
+    public class GameBoardModelTests
+    {
+        [TestMethod()]
+        public void GameBoardModelTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCellTest()
+        {
+            GameBoardModel board = new GameBoardModel(3);
+            char result = board.GetCell(1, 2);
+            Assert.AreEqual(' ', result);
+        }
+
+        
+    }
+}
